502038833df2ed96be487d241bf7781da12390eb
Dhanush Tharan M <106361969+Dhanushtharan@users.noreply.github.com>
2024-02-19 16:26:55 +0530

    Add files via upload

---
+++ front-end/Day1/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Login from './components/login';
+
+function App(){
+  return(
+    <div className='App'>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
