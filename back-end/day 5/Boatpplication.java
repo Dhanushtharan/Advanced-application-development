5d8e9c9e878ea6ea67f62c2ad5cc469d22d9135e
Dhanush Tharan M <106361969+Dhanushtharan@users.noreply.github.com>
2024-03-05 12:08:28 +0530

    Update Boatpplication.java

--- back-end/day 5/Boatpplication.java
+++ back-end/day 5/Boatpplication.java
@@ -1,4 +1,4 @@
-package com.example.birthdayback;
+package com.example.boatApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
